852929a79b7880c1eff44acbd607315d30c42627
Ray Pan <raypan@RaydeMacBook-Pro.local>
2015-05-03 03:45:10 +0800

    安裝compass並compile資料夾裡的sass檔案，產生style.css

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
+
 
 var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = [
@@ -11,6 +13,8 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 	];
+
+var sassSources = ['components/sass/style.scss'];
 
 
 gulp.task('coffee', function(){
@@ -29,4 +33,17 @@ gulp.task('js' , function(){
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass' , function(){
+	gulp.src(sassSources)
+		.pipe(compass(
+			 {
+			 	sass: 'components/sass',
+			 	image : 'builds/development/images',
+			 	style: 'expanded'
+			 }
+			)
+		.on('error' , gutil.log))
+		.pipe(gulp.dest('builds/development/css'))
 });
