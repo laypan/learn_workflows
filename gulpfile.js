3dffc579304f27fefd61a7a20ec7ef48817c12b7
Ray Pan <raypan@RaydeMacBook-Pro.local>
2015-05-03 04:33:27 +0800

    增加對html、json的livereload

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,8 @@ var jsSources = [
 	];
 
 var sassSources = ['components/sass/style.scss'];
-
-
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
 		.pipe(
@@ -55,6 +55,8 @@ gulp.task('watch' , function(){
 	gulp.watch(coffeeSources,['coffee']);
 	gulp.watch(jsSources,['js']);
 	gulp.watch('components/sass/*.scss',['compass']);
+	gulp.watch(htmlSources , ['html']);
+	gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect' , function(){
@@ -64,6 +66,15 @@ gulp.task('connect' , function(){
 	});
 });
 
+gulp.task('html' , function(){
+	gulp.src(htmlSources)
+	  .pipe(connect.reload())
+});
 
-gulp.task('default',['coffee','js','compass','connect','watch']);
+gulp.task('json' , function(){
+	gulp.src(jsonSources)
+	  .pipe(connect.reload())
+});
+
+gulp.task('default',['coffee','js','compass','json','connect','watch']);
 
