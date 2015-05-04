84c65ee0686ba34cc32b42cac1e1e1647811f925
Ray Pan <raypan@RaydeMacBook-Pro.local>
2015-05-04 12:34:50 +0800

    加入gulp-minify-html，讓html輕量化。

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 
 // 定義所需組譯程式的參數
@@ -90,7 +91,7 @@ gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
-    gulp.watch(htmlSources, ['html']);
+    gulp.watch('builds/development/*.html', ['html']);
     gulp.watch(jsonSources, ['json']);
 });
 // 使用gulp-connect，並加入livereload功能讓瀏覽器能夠監聽檔案自動重整網頁，outputDir是NODE_ENV設定的builds/development/
@@ -102,7 +103,9 @@ gulp.task('connect', function() {
 });
 // 因為html以及json不需要解譯，他只要儲存就好，所以單獨設定他儲存後就可以重整
 gulp.task('html', function() {
-    gulp.src(htmlSources)
+    gulp.src('builds/development/*.html')
+    	.pipe(gulpif(env === 'production' , minifyHTML()))
+    	.pipe(gulpif(env === 'production' , gulp.dest(outputDir)))
         .pipe(connect.reload())
 });
 // 因為html以及json不需要解譯，他只要儲存就好，所以單獨設定他儲存後就可以重整
